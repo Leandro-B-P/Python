124dfee309d821ada0e1e14b0ac1d74f841782b0
Leandro B Pereira <leandrobatistapereira98@gmail.com>
2016-09-21 22:29:31 -0300

    Find pages for admin

---
+++ find_admin.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# encoding: utf-8
+import requests
+
+
+paginas = []
+site = raw_input('Digite a url do site: ')
+directory_list = raw_input('Digite a sua wordlist: ')
+direc_list = open(directory_list, 'r')
+diretorios = direc_list.readlines()
+direc_list.close()
+
+for diretorio in diretorios:
+    diretorio = diretorio.replace('\n', '')
+    barra = site + '/' + diretorio
+    r = requests.get(barra)
+    status = r.status_code
+    if status != 301 and status != 404:
+       if not "Page not found" in r.content:
+          print "[+] PAGINA ENCONTRADA!: " + barra
+          paginas.append(r)
+       else:
+           print "[-] PAGE NOT FOUND: " + barra
+    else:
+        print "[-] PAGE NOT FOUND: " + barra
+
+for pagina in paginas:
+    print pagina
