51247464ec28e0958762c5a4e9ae818843a8641c
Leandro B Pereira <leandrobatistapereira98@gmail.com>
2016-09-26 21:42:54 -0300

    Traz sua longitude e latitude

---
+++ latitude.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import requests
+
+
+def geocode(address):
+    parameters = {'address': address, 'sensor': 'false'}
+    base = 'http://maps.googleapis.com/maps/api/geocode/json'
+    response = requests.get(base, params=parameters)
+    answer = response.json()
+    print(answer['results'][0]['geometry']['location'])
+
+
+if __name__ == '__main__':
+    long_lat = input('Digite o endereco para pegar a latitude e longitude: ')
+    geocode(long_lat)
