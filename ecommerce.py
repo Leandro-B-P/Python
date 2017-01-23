6ff3e1798c5a9e5fb197fc4b1ea4f74073abe172
Leandro B Pereira <leandrobatistapereira98@gmail.com>
2017-01-23 00:54:04 -0200

    Add files via upload

---
+++ ecommerce.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+# Author: leandrobatistapereira98@gmail.com
+#
+carrinho = []
+
+produto1 = {'nome': 'Tenis', 'valor': 21.70}
+produto2 = {'nome': 'Meia', 'valor': 10}
+produto3 = {'nome': 'Camiseta', 'valor': 17.30}
+produto4 = {'nome': 'Calca', 'valor': 300.00}
+
+carrinho.append(produto1)
+carrinho.append(produto2)
+carrinho.append(produto3)
+carrinho.append(produto4)
+
+print "Seu carrinho possui ", len(carrinho), " itens."
+total = 0
+for c in carrinho:
+    total += c['valor']
+
+print 'O valor total e de ', total
