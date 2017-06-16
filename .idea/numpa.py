420191e35f4acc2a090f84d885b47c29626fceb6
Leandro <leandrobatistapereira98@gmail.com>
2017-06-15 22:31:50 -0300

    Estrutura da atribuição condicional

---
+++ .idea/numpa.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python
+# Estrutura da atribuição condicional
+
+num1 = int(input('Digite um numero: '))
+s = 'par' if num1 % 2 == 0 else 'impar'
+print('O numero digitado é', s)
